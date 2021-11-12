18519293e0e52cb10d49e293dc2b7d9f11ca6aff
black1ava <black1ava@github.com>
2021-11-13 02:40:18 +0700

    Add dropdown

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,22 @@
-console.log('Hello');
+window.onload = function(){
+  const anchors = document.getElementsByTagName('a');
+  const dropdowns = Array.prototype.filter.call(anchors, function(anchor){
+    return anchor.getAttribute('toggle-type') === 'dropdown';
+  });
+
+  dropdowns.map(function(dropdown){
+    const icons = document.getElementsByClassName('dropdown-icon');
+    const icon = Array.prototype.filter.call(icons, function(icon){
+      return icon.getAttribute('toggle-for') === dropdown.getAttribute('id');
+    });
+
+    if(icon.length === 0) return null;
+
+    let count = 1;
+
+    dropdown.addEventListener('click', function(){
+      icon[0].style.transform = `rotate(${ (count % 2) * 180 }deg)`;
+      count++;
+    });
+  });
+}
