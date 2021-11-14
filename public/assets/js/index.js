cd6e8c92525161c7b561ec3d372b01b4c227d8b8
black1ava <black1ava@github.com>
2021-11-14 17:58:39 +0700

    Add dropdown

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,11 +3,11 @@ window.onload = function(){
   const mainContent = document.getElementsByClassName('main-content');
 
   const anchors = document.getElementsByClassName('topbar-dropdown');
-  const dropdowns = Array.prototype.filter.call(anchors, function(anchor){
+  const topbarDropdowns = Array.prototype.filter.call(anchors, function(anchor){
     return anchor.getAttribute('toggle-type') === 'dropdown';
   });
 
-  dropdowns.map(function(dropdown){
+  topbarDropdowns.map(function(dropdown){
     const icons = document.getElementsByClassName('dropdown-icon');
     const icon = Array.prototype.filter.call(icons, function(icon){
       return icon.getAttribute('toggle-for') === dropdown.getAttribute('id');
@@ -18,18 +18,15 @@ window.onload = function(){
       return content.getAttribute('content-of') === dropdown.getAttribute('id');
     });
 
-    let count = 1;
     dropdown.addEventListener('click', function(){
 
       if(icon.length > 0){
-        icon[0].style.transform = `rotate(${ (count % 2) * 180 }deg)`;
+        icon[0].classList.toggle('active');
       }
 
       if(content.length > 0){
         content[0].classList.toggle('topbar-dropdown-content-active');
       }
-
-      count++;
     });
   });
 
@@ -60,6 +57,29 @@ window.onload = function(){
       darkScreen.classList.toggle('active');
       sidebar[0].classList.toggle('active');
       mainContent[0].classList.toggle('side-bar-active');
+    });
+  });
+
+  const dropdowns = document.getElementsByClassName('dropdown');
+  Array.prototype.map.call(dropdowns, function(dropdown){
+    const icons = document.getElementsByClassName('dropdown-icon');
+    const icon = Array.prototype.filter.call(icons, function(icon){
+      return icon.getAttribute('toggle-for') === dropdown.getAttribute('id');
+    });
+
+    const menus = document.getElementsByClassName('dropdown-menu');
+    const menu = Array.prototype.filter.call(menus, function(menu){
+      return menu.getAttribute('content-of') === dropdown.getAttribute('id');
+    });
+
+    dropdown.addEventListener('click', function(){
+      if(icon.length > 0){
+        icon[0].classList.toggle('active');
+      }
+
+      if(menu.length > 0){
+        menu[0].classList.toggle('active');
+      }
     });
   });
 }
